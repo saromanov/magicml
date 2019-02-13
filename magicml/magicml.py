f24442ee509129e2c4e82e6337e17a0619831a0a
Sergey <xxsmotur@gmail.com>
2019-02-13 18:40:13 +0500

    splitting of dataset

--- magicml/magicml.py
+++ magicml/magicml.py
@@ -4,3 +4,5 @@ class TextClassification:
     """ Applying of the text classification"""
     def __init__(self, dataset):
         self._dataset = dataset
+        self.train_df = load_dataset(os.path.join(os.path.dirname(dataset),"aclImdb", "train"))
+        self.test_df = load_dataset(os.path.join(os.path.dirname(dataset),"aclImdb", "test"))
